8a6ea225044956d97fbfcd27b0f71a4ce3d98e68
Douglas feliphe <douglasfeliphe@hotmail.com>
2022-01-11 07:18:53 +0000

    mais ajustes

--- main.py
+++ main.py
@@ -1,47 +1,86 @@
 import telebot
-from telebot import types
+# from telebot import types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 username = ''
 confirmEmail = False
 email = ''
 password = ''
-
+msg = None
 # You can set parse_mode by default. HTML or MARKDOWN
 bot = telebot.TeleBot("2109357146:AAGRjkbIg0I5gy3kqNV_2G1AWW85xvyahjg") 
 
-@bot.message_handler(commands=['start', 'help'])
+user_dict = {}
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.age = None
+        self.sex = None
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-  username = message.from_user.first_name  
-  bot.send_message(message.chat.id, f"Ola @{username} este é o ENTREVIBOT. Informe seu email por favor?")  
-
-@bot.message_handler(func=lambda message: True)
-def get_email(message):
-  email = message.text
-  markup = types.ReplyKeyboardMarkup(row_width=2)
-  itembtn1 = types.KeyboardButton('Sim')
-  itembtn2 = types.KeyboardButton('Não')  
-  markup.add(itembtn1, itembtn2)  
-  bot.send_message(message.chat.id, f'Ótimo! Anotei seu email como {email}. Está correto?', reply_markup=markup)
-
-@bot.message_handler(func=lambda message: True)
-def confirm_email(message):  
-  bot.send_message(message.chat.id, f'Agora, informe uma senha de acesso (fique tranquilo nossas mensagens são criptografadas)', reply_markup=markup)  
-
-# @bot.message_handler(func=lambda message: True)
-# def get_password(message): 
-#   bot.send_message(message.chat.id, f' Agora, informe uma senha de acesso (fique tranquilo nossas mensagens são criptografadas)')
-#   password = message.text
-#   markup = types.ReplyKeyboardMarkup(row_width=2)
-#   itembtn1 = types.KeyboardButton('Sim')
-#   itembtn2 = types.KeyboardButton('Não')  
-#   markup.add(itembtn1, itembtn2) 
-#   bot.send_message(message.chat.id, f'Ótimo! Anotei seu email como {email}. Está correto?', reply_markup=markup)
+    msg = bot.reply_to(message, """\
+Hi there, I am Example bot.
+What's your name?
+""")
+    bot.register_next_step_handler(msg, process_name_step)
 
 
+def process_name_step(message):
+    try:
+        chat_id = message.chat.id
+        name = message.text
+        user = User(name)
+        user_dict[chat_id] = user
+        msg = bot.reply_to(message, 'How old are you?')
+        bot.register_next_step_handler(msg, process_age_step)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
 
 
+def process_age_step(message):
+    try:
+        chat_id = message.chat.id
+        age = message.text
+        if not age.isdigit():
+            msg = bot.reply_to(message, 'Age should be a number. How old are you?')
+            bot.register_next_step_handler(msg, process_age_step)
+            return
+        user = user_dict[chat_id]
+        user.age = age
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Male', 'Female')
+        msg = bot.reply_to(message, 'What is your gender', reply_markup=markup)
+        bot.register_next_step_handler(msg, process_sex_step)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
 
 
-print('bot rodando')
+def process_sex_step(message):
+    try:
+        chat_id = message.chat.id
+        sex = message.text
+        user = user_dict[chat_id]
+        if (sex == u'Male') or (sex == u'Female'):
+            user.sex = sex
+        else:
+            raise Exception("Unknown sex")
+        bot.send_message(chat_id, 'Nice to meet you ' + user.name + '\n Age:' + str(user.age) + '\n Sex:' + user.sex)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
 
-bot.infinity_polling()  
+
+# Enable saving next step handlers to file "./.handlers-saves/step.save".
+# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
+# saving will hapen after delay 2 seconds.
+bot.enable_save_next_step_handlers(delay=2)
+
+# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+# WARNING It will work only if enable_save_next_step_handlers was called!
+bot.load_next_step_handlers()
+
+bot.infinity_polling()
