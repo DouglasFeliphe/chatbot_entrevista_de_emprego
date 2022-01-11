8694eb0e9cf3a87b554413aaeda48b6b3d9ea48b
Douglas feliphe <douglasfeliphe@hotmail.com>
2022-01-11 05:12:35 +0000

    mais ajustes

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import telebot
+from telebot import types
 
 username = ''
 confirmEmail = False
 email = ''
+password = ''
 
 # You can set parse_mode by default. HTML or MARKDOWN
-bot = telebot.TeleBot("2109357146:AAGRjkbIg0I5gy3kqNV_2G1AWW85xvyahjg", parse_mode=None) 
+bot = telebot.TeleBot("2109357146:AAGRjkbIg0I5gy3kqNV_2G1AWW85xvyahjg") 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
   username = message.from_user.first_name  
-  bot.reply_to(message, f"Ola @{username} este é o ENTREVIBOT. Informe seu email por favor?")  
+  bot.send_message(message.chat.id, f"Ola @{username} este é o ENTREVIBOT. Informe seu email por favor?")  
 
 @bot.message_handler(func=lambda message: True)
 def get_email(message):
   email = message.text
-  bot.reply_to(message, f'Ótimo! Anotei seu email como {email}. Está correto? \n/sim\n/nao')
+  markup = types.ReplyKeyboardMarkup(row_width=2)
+  itembtn1 = types.KeyboardButton('Sim')
+  itembtn2 = types.KeyboardButton('Não')  
+  markup.add(itembtn1, itembtn2)  
+  bot.send_message(message.chat.id, f'Ótimo! Anotei seu email como {email}. Está correto?', reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
+def confirm_email(message):  
+  bot.send_message(message.chat.id, f'Agora, informe uma senha de acesso (fique tranquilo nossas mensagens são criptografadas)', reply_markup=markup)  
+
+# @bot.message_handler(func=lambda message: True)
+# def get_password(message): 
+#   bot.send_message(message.chat.id, f' Agora, informe uma senha de acesso (fique tranquilo nossas mensagens são criptografadas)')
+#   password = message.text
+#   markup = types.ReplyKeyboardMarkup(row_width=2)
+#   itembtn1 = types.KeyboardButton('Sim')
+#   itembtn2 = types.KeyboardButton('Não')  
+#   markup.add(itembtn1, itembtn2) 
+#   bot.send_message(message.chat.id, f'Ótimo! Anotei seu email como {email}. Está correto?', reply_markup=markup)
+
+
+
+
 
 
 print('bot rodando')
