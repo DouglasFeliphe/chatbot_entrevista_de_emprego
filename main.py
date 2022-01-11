1e0830c31e608544a77e5d89348ab41f3868ba57
Douglas feliphe <douglasfeliphe@hotmail.com>
2022-01-11 01:28:16 +0000

    ajustes

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-
 import telebot
 
-bot = telebot.TeleBot("2109357146:AAGRjkbIg0I5gy3kqNV_2G1AWW85xvyahjg")
+username = ''
+email = ''
 
-firstTime = True
+bot = telebot.TeleBot("2109357146:AAGRjkbIg0I5gy3kqNV_2G1AWW85xvyahjg", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):  
-	bot.reply_to(message, "Howdy, how are you doing?")
+def send_welcome(message):
+	bot.reply_to(message, "Ola este é o BOT. Informe seu email por favor?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, "Ola este é o BOT. Informe seu email por favor: ")
+	bot.reply_to(message, message.text)    
 
-  if message=='teste@teste.com'
-  # firstTime = False
+@bot.message_handler(regexp="SOME_REGEXP")
+def handle_message(message):  
+		bot.reply_to(message, "regex?")  
 
-@bot.message_handler(func=lambda message: True)
-def function_name(message):
-  if re.search(r'@|.', message.text) and not firstTime:
-	  bot.reply_to(message, "This is a message handler")
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+	bot.reply_to(message, "Howdy, how are you doing?")    
 
+print('bot rodando')
 
-print("Bot rodando...")
-
-bot.infinity_polling()
+bot.infinity_polling()  
